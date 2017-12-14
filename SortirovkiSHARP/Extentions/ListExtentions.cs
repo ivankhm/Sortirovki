63c34abba4d73fad58c23b15349895a0a77c69e6
Narutlih <godpingwiner@gmail.com>
2017-12-14 20:26:49 +0300

    Mamka

--- SortirovkiSHARP/Extentions/ListExtentions.cs
+++ SortirovkiSHARP/Extentions/ListExtentions.cs
@@ -21,5 +21,12 @@ namespace SortirovkiSHARP.Extentions
                 list[n] = value;
             }
         }
+
+        public static void Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
     }
 }
